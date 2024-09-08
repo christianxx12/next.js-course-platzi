fcb086bdf05855ee3cdd78023a96a4cab8f728ae
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-09-08 17:24:06 -0500

    Creando la variables de entorno y obteniendo los datos de la api de shopify

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,21 @@
-export function MainProducts() {
+async function getProducts() {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    {
+      headers: {
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
+      },
+    }
+  )
+  const data = await response.json()
+
+  return data
+}
+
+export async function MainProducts() {
+  const products = await getProducts()
+  console.log(products)
+
   return (
     <section>
       <h1>MainProducts</h1>
